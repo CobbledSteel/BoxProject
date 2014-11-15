b610d70d62c6d0ae18368a6ca4140e408d7f708b
CobbledSteel <vadimnikiforov2@hunterschools.org>
2014-11-14 21:42:53 -0500

    Options added 0.0

---
+++ Options.java
@@ -0,0 +1,76 @@
+
+public class Options 
+{
+
+	private static double spazRate;
+	private static double xAverage;
+	private static double yAverage;
+	private static double colorChangeRate;
+	private static double speed;
+	private static double turnRate;
+	private static double waitTime;
+	
+	public static void setSpazRate(double s)
+	{
+		spazRate = s;
+	}
+	public static double getSpazRate()
+	{
+		return spazRate;
+	}
+	
+	public static void setXAverage(double b)
+	{
+		xAverage = b;
+	}
+	public static double getXAverage()
+	{
+		return xAverage;
+	}
+	
+	public static void setYAverage(double b)
+	{
+		yAverage = b;
+	}
+	public static double getYAverage()
+	{
+		return yAverage;
+	}
+	
+	public static void setColorChangeRate(double c)
+	{
+		colorChangeRate = c;
+	}
+	public static double getColorChangeRate()
+	{
+		return colorChangeRate;
+	}
+	
+	public static void setSpeed(double s)
+	{
+		speed = s;
+	}
+	public static double getSpeed()
+	{
+		return speed;
+	}
+	
+	public static void setTurnRate(double t)
+	{
+		turnRate = t;
+	}
+	public static double getTurnRate()
+	{
+		return turnRate;
+	}
+	
+	public static void setwaitTime(double f)
+	{
+		waitTime = f;
+	}	
+	public static double getWaitTime()
+	{
+		return waitTime;
+	}
+	
+}
